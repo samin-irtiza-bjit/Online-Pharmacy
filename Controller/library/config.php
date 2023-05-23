c0372f5eae3151fb3a14a0eaba7709a7a96b1e19
Md. Samin Irtiza <131145579+samin-irtiza-bjit@users.noreply.github.com>
2023-05-23 14:58:17 +0600

    Update config.php

--- Controller/library/config.php
+++ Controller/library/config.php
@@ -9,8 +9,8 @@
         define('DB_DATABASE', 'db_pharmacy');
     } else {
         define('DB_SERVER', 'localhost');
-        define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
+        define('DB_USERNAME', 'pharma');
+        define('DB_PASSWORD', 'pharmapass');
         define('DB_DATABASE', 'db_pharmacy');
     }
     if(!mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)){
